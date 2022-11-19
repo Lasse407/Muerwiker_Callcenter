d4a0325451974eb4f8816a97590ca0de2f4a9bac
NoahWunderlich <noah.Wunderlich3@googlemail.com>
2022-11-19 16:53:18 +0100

    WorkTable weiter optimiert Anreihung der Tabelle geändert

--- src/components/WorkTable.vue
+++ src/components/WorkTable.vue
@@ -1,22 +1,30 @@
 <template>
-  <div v-for="(item,index) in getCities()" :key="index">
-    <p  @click="selectedCity = item" style="background-color: lightblue; margin: 4px">{{item}}</p>
+  <div style="width: 300px; height: auto; float: left;">
+    <div v-for="(item,index) in getCities()" :key="index">
+      <p @click="selectedCity = item" style="background-color: lightblue; margin: 4px; ">{{ item }}</p>
+    </div>
   </div>
-  <p>-----------------------------------------------------------------</p>
+
   <span v-if="selectedCity!= ''">
-    <div v-for="(item, index) in getAddresses()" :key="index" style="background-color: lightblue; margin: 4px">
+      <div style="width: 300px; height: auto; float: left;">
+    <div v-for="(item, index) in getAddresses()" :key="index"
+         style="background-color: lightblue; margin: 4px; width: 300px; height: auto; float: left;">
       <div @click="selectedAddress = item.id">
-    <p>{{item.strasse}}</p>
-    <p>{{item.hausnummer}}</p>
-    <p>{{item.abteilungen}}</p>
+    <p>{{ item.strasse }}</p>
+    <p>{{ item.hausnummer }}</p>
+    <p>{{ item.abteilungen }}</p>
 </div>
+    </div>
 </div>
   </span>
-  <p>-----------------------------------------------------------------</p>
   <span v-if="selectedAddress!= ''">
-    <div v-for="(item, index) in getMitarbeiter()" :key="index" style="background-color: lightblue; margin: 4px">
-    <p>{{item.name}}</p>
-    <p>{{item.nummer}}</p>
+          <div style="width: 300px; height: auto; float: left;">
+
+    <div v-for="(item, index) in getMitarbeiter()" :key="index"
+         style="background-color: lightblue; margin: 4px; width: 300px; height: auto; float: left;">
+    <p>{{ item.name }}</p>
+    <p>{{ item.nummer }}</p>
+    </div>
 </div>
   </span>
 </template>
@@ -25,90 +33,91 @@
 
 export default {
   name: "WorkTable",
-  data(){
+  data() {
     return {
-      selectedCity:"",
-      selectedAddress:"",
-      workData:{
-       "arbeitsstaetten":[
-           {
-            id:1,
-            ort:"Flensburg",
-            strasse:"testStraße",
-            hausnummer:"69",
-            abteilungen:"Holz, Metall"
+      selectedCity: "",
+      selectedAddress: "",
+      workData: {
+        "arbeitsstaetten": [
+          {
+            id: 1,
+            ort: "Flensburg",
+            strasse: "testStraße",
+            hausnummer: "69",
+            abteilungen: "Holz, Metall"
           },
           {
-            id:2,
-            ort:"Flensburg",
-            strasse:"Nudelweg",
-            hausnummer:"88",
-            abteilungen:"Schleudern, Pumpen"
+            id: 2,
+            ort: "Flensburg",
+            strasse: "Nudelweg",
+            hausnummer: "88",
+            abteilungen: "Schleudern, Pumpen"
           },
-         {
-           id:3,
-           ort:"Niebüll",
-           strasse:"Nordseestraße",
-           hausnummer:"11",
-           abteilungen:"Dies, Das"
-         }
+          {
+            id: 3,
+            ort: "Niebüll",
+            strasse: "Nordseestraße",
+            hausnummer: "11",
+            abteilungen: "Dies, Das"
+          }
         ]
       },
-      contactData:{
-        "mitarbeiter":[
+      contactData: {
+        "mitarbeiter": [
           {
-            id:1,
-            zuordnungsId:1,
-            name:"Peter Petersen",
-            nummer:"xxx xxx"
-          },{
-            id:2,
-            zuordnungsId:1,
-            name:"Hans Hansen",
-            nummer:"xxx xxx"
-          },{
-            id:3,
-            zuordnungsId:2,
-            name:"Johann Johannsen",
-            nummer:"xxx xxx"
-          },{
-            id:4,
-            zuordnungsId:2,
-            name:"Erich Erichsen",
-            nummer:"xxx xxx"
-          },{
-            id:5,
-            zuordnungsId:3,
-            name:"Michel Michelsen",
-            nummer:"xxx xxx"
+            id: 1,
+            zuordnungsId: 1,
+            name: "Peter Petersen",
+            nummer: "xxx xxx"
+          }, {
+            id: 2,
+            zuordnungsId: 1,
+            name: "Hans Hansen",
+            nummer: "xxx xxx"
+          }, {
+            id: 3,
+            zuordnungsId: 2,
+            name: "Johann Johannsen",
+            nummer: "xxx xxx"
+          }, {
+            id: 4,
+            zuordnungsId: 2,
+            name: "Erich Erichsen",
+            nummer: "xxx xxx"
+          }, {
+            id: 5,
+            zuordnungsId: 3,
+            name: "Michel Michelsen",
+            nummer: "xxx xxx"
           }
         ]
       }
     }
   },
-  mounted(){
-      console.log(this.workData.arbeitsstaetten);
+  mounted() {
+    console.log(this.workData.arbeitsstaetten);
   },
-  methods:{
-    write(){
+  methods: {
+    write() {
       console.log("writing -------------------")
     },
-    getCities(){
+    getCities() {
       var resArr = [];
       this.workData.arbeitsstaetten.forEach((element) => {
 
-        if(!resArr.includes(element.ort)){
+        if (!resArr.includes(element.ort)) {
 
           resArr.push(element.ort)
 
         }
       })
+
       return resArr;
     },
-    getAddresses(){
+    getAddresses() {
       var resArr = [];
       this.workData.arbeitsstaetten.forEach((element) => {
-        if(element.ort == this.selectedCity){
+        if (element.ort == this.selectedCity) {
           resArr.push(element)
           console.log("pushed element:")
           console.log(element)
@@ -116,10 +125,10 @@ export default {
       })
       return resArr;
     },
-    getMitarbeiter(){
+    getMitarbeiter() {
       var resArr = [];
       this.contactData.mitarbeiter.forEach((element) => {
-        if(element.zuordnungsId == this.selectedAddress){
+        if (element.zuordnungsId == this.selectedAddress) {
           resArr.push(element)
           console.log("pushed element:")
           console.log(element)
